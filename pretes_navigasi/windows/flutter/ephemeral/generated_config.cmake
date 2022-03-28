d84020db6b7f2c02cfa45249f6d077f2646800f8
hafidz313 <lefet313@gmail.com>
2022-03-28 07:16:00 +0700

    Pretest1

---
+++ pretes_navigasi/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\flutter\\pretes_navigation\\pretes_navigasi" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter\\flutter"
+  "PROJECT_DIR=D:\\flutter\\pretes_navigation\\pretes_navigasi"
+  "FLUTTER_ROOT=D:\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\flutter\\pretes_navigation\\pretes_navigasi\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\flutter\\pretes_navigation\\pretes_navigasi"
+  "FLUTTER_TARGET=D:\\flutter\\pretes_navigation\\pretes_navigasi\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\flutter\\pretes_navigation\\pretes_navigasi\\.dart_tool\\package_config.json"
+)
